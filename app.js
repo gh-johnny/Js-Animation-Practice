57f59dd1906695ad154299c892ea01b611b45014
Marcelo <joaomarcelofurtadoromero@gmail.com>
2022-11-12 04:10:14 -0800

    The background for the about section and it's animation is done, would still have to trigger only when on the page to make it lighter, it's animation to change the navigation sidebar icons color is also done

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+let homeBtn = document.querySelector('.fa-house');
+let aboutBtn = document.querySelector('.fa-user');
+let skillsBtn = document.querySelector('.fa-code');
+let contactBtn = document.querySelector('.fa-at');
+let bar = document.querySelector('ul > aside');
+let navNewColor = '#fff';
+let stars = document.querySelectorAll('.stars');
 //GSAP
 gsap.registerPlugin(ScrollTrigger);
 //Main scroll behavior
@@ -7,8 +14,56 @@ gsap.utils.toArray(".panel").forEach((panel, i) => {
         start: "top top", 
         pin: true, 
         pinSpacing: false,
-        snap: 4 / 4,
+        snap: 1 / 1,
     });
 });
-//About
+//About section
+//Navigation colors
+let tl = gsap.timeline({defaults: {color: navNewColor}})
+.to(bar,{backgroundColor: navNewColor})
+.to(contactBtn,{})
+.to(skillsBtn,{})
+.to(aboutBtn,{})
+.to(homeBtn,{})
+ScrollTrigger.create({
+    trigger: '.panel about',
+    start: 'top top',
+    scrub: true,
+    animation: tl,
+    // markers: true
+});
+//Background stars
+// let starTl = gsap.timeline()
+// .to('#stars1',{x:-3500, duration: 50})
+// .to('#stars2',{x:-3500, duration: 100})
+// .to('#stars3',{x:-3500, duration: 135});
+// ScrollTrigger.create({
+//     trigger: '.panel about',
+//     start: 'top top',
+//     animation: starTl
+// })
+gsap.fromTo('#stars1',{x: window.innerWidth},{x:-3500, duration: 50});
+gsap.fromTo('#stars2',{x: window.innerWidth},{x:-3500, duration: 100});
+gsap.fromTo('#stars3',{x: window.innerWidth},{x:-3500, duration: 135});
+
+
+
+//Skills section
+//Animation
+// .to(homeBtn,{color: 'transparent'})
+// .to(homeBtn,{})
+// .to(aboutBtn,{color: 'transparent'})
+// .to(aboutBtn,{})
+// .to(skillsBtn,{color: 'transparent'})
+// .to(skillsBtn,{})
+// .to(contactBtn,{color: 'transparent'})
+// .to(contactBtn,{})
+// .to(bar,{backgroundColor: 'transparent'})
+// .to(bar,{backgroundColor: navNewColor})
+
+
+
+
+
+
 
