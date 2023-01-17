83ddbe57e7744467d5d5a6f216f9b07c5a83f124
Marcelo <joaomarcelofurtadoromero@gmail.com>
2023-01-17 06:23:34 -0800

    Main Branch: Contact section done + some adjustments + anchor tag fixed

--- app.js
+++ app.js
@@ -1,150 +1,183 @@
 //Variables
 let homeBtn = $('.fa-house'),
-    aboutBtn = $('.fa-user'),
-    projectsBtn = $('.fa-code'),
-    contactBtn = $('.fa-at'),
-    bar = $('ul > aside'),
-    navLightColor = '#fff',
-    navDarkColor = 'rgb(20, 20, 20)',
-    navPickedColor = '#04af65',
-    listLi = $('.about-container li'),
-    slide = $('.slide');
-    labels = $('.selector'),
-    sphere = $('.sphere');
+  aboutBtn = $('.fa-user'),
+  projectsBtn = $('.fa-code'),
+  contactBtn = $('.fa-at'),
+  bar = $('ul > aside'),
+  navLightColor = '#fff',
+  navDarkColor = 'rgb(20, 20, 20)',
+  navPickedColor = '#04af65',
+  listLi = $('.about-container li'),
+  slide = $('.slide'),
+  labels = $('.selector'),
+  sphere = $('.sphere'),
+  clipboardIcon = $('.icon-holder')
 //Default states
-homeBtn.css('color',navPickedColor);
-slide.hide();
-$(slide[0]).show();
+homeBtn.css('color', navPickedColor)
+slide.hide()
+$(slide[0]).show()
 $(labels[0]).toggleClass('selected')
-listLi.addClass('uncheck');
-sphere.css('color',navLightColor);
+listLi.addClass('uncheck')
+sphere.css('color', navLightColor)
 //Main scroll behavior
-gsap.registerPlugin(ScrollTrigger);
-gsap.utils.toArray(".panel").forEach((panel) => {
-    ScrollTrigger.create({
-        trigger: panel,
-        start: "top top", 
-        // pin: true, 
-        pinSpacing: false,
-        snap: 1 / 1,
-    });
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
+gsap.utils.toArray('.panel').forEach(panel => {
+  ScrollTrigger.create({
+    trigger: panel,
+    start: 'top top',
+    // pin: true,
+    pinSpacing: false,
+    snap: 1 / 1
+  })
+})
+//Scroll to
+document.querySelectorAll("nav a").forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    gsap.to(window, {duration: 1, scrollTo:{y:`#section${index + 1}`}});
+  });
 });
 //About section
 //Navigation bar colors animation
-let aboutNavBarTl = gsap.timeline({defaults: {color: navLightColor}})
-.to(bar,{backgroundColor: navLightColor})
-.to(contactBtn,{})
-.to(projectsBtn,{})
-.to(aboutBtn,{color: navPickedColor})
-.to(homeBtn,{})
-.to(bar,{backgroundColor: navLightColor})
+let aboutNavBarTl = gsap
+  .timeline({ defaults: { color: navLightColor } })
+  .to(bar, { backgroundColor: navLightColor })
+  .to(contactBtn, {})
+  .to(projectsBtn, {})
+  .to(aboutBtn, { color: navPickedColor })
+  .to(homeBtn, {})
+  .to(bar, { backgroundColor: navLightColor })
 ScrollTrigger.create({
-    trigger: '.marker-about',
-    start: 'top bottom',
-    scrub: true,
-    animation: aboutNavBarTl,
-});
+  trigger: '.marker-about',
+  start: 'top bottom',
+  scrub: true,
+  animation: aboutNavBarTl
+})
 //Background stars animation
-gsap.fromTo('#stars1',{x: window.innerWidth},{x:-1500, duration: 30});
-gsap.fromTo('#stars2',{x: window.innerWidth},{x:-1500, duration: 60});
-gsap.fromTo('#stars3',{x: window.innerWidth},{x:-1500, ease: 'power1.out',duration: 85});
+gsap.fromTo('#stars1', { x: window.innerWidth }, { x: -1500, duration: 30 })
+gsap.fromTo('#stars2', { x: window.innerWidth }, { x: -1500, duration: 60 })
+gsap.fromTo(
+  '#stars3',
+  { x: window.innerWidth },
+  { x: -1500, ease: 'power1.out', duration: 85 }
+)
 //Flotating animation
-const floatingAnimation = function(target,direction,axis,duration=2){
-    (axis=='y') 
-    ?
-        gsap.timeline({repeat:-1,defaults:{duration:duration,ease:'power1.inOut'}})
-        .fromTo(target,{y:`+=${direction}`},{y:`-=${direction}`})
-        .fromTo(target,{y:`-=${direction}`},{y:`+=${direction}`})
-    :
-        gsap.timeline({repeat:-1,defaults:{duration:duration,ease:'power1.inOut'}})
-        .fromTo(target,{x:`+=${direction}`},{x:`-=${direction}`})
-        .fromTo(target,{x:`-=${direction}`},{x:`+=${direction}`})
+const floatingAnimation = function (target, direction, axis, duration = 2) {
+  axis == 'y'
+    ? gsap
+        .timeline({
+          repeat: -1,
+          defaults: { duration: duration, ease: 'power1.inOut' }
+        })
+        .fromTo(target, { y: `+=${direction}` }, { y: `-=${direction}` })
+        .fromTo(target, { y: `-=${direction}` }, { y: `+=${direction}` })
+    : gsap
+        .timeline({
+          repeat: -1,
+          defaults: { duration: duration, ease: 'power1.inOut' }
+        })
+        .fromTo(target, { x: `+=${direction}` }, { x: `-=${direction}` })
+        .fromTo(target, { x: `-=${direction}` }, { x: `+=${direction}` })
 }
-floatingAnimation('.austronaut',15,'y');
-floatingAnimation('.pencil',12,'y',3);
-floatingAnimation('.me-coding',13,'y',3);
-floatingAnimation('.me-coding',35,'x',5);
+floatingAnimation('.austronaut', 15, 'y')
+floatingAnimation('.pencil', 12, 'y', 3)
+floatingAnimation('.me-coding', 13, 'y', 3)
+floatingAnimation('.me-coding', 35, 'x', 5)
 //Crossing out post-it and showing answer
-$(function(){
-    $('.about-container li').click(function(){
-        $(this).toggleClass('uncheck');
-        switch($(this).index()){
-            case 0:
-                $('.one').toggle()
-            break
-            case 1:
-                $('.two').toggle()
-            break
-            case 2:
-                $('.three').toggle()
-            break
-            case 3:
-                $('.four').toggle()
-            break
-            case 4:
-                $('.five').toggle()
-            break
-            case 5:
-                $('.six').toggle()
-            break
-        }
-    });
-});
+$(function () {
+  $('.about-container li').click(function () {
+    $(this).toggleClass('uncheck')
+    switch ($(this).index()) {
+      case 0:
+        $('.one').toggle()
+        break
+      case 1:
+        $('.two').toggle()
+        break
+      case 2:
+        $('.three').toggle()
+        break
+      case 3:
+        $('.four').toggle()
+        break
+      case 4:
+        $('.five').toggle()
+        break
+      case 5:
+        $('.six').toggle()
+        break
+    }
+  })
+})
 //Slide effect
 let slidePicker = $('.slide-picker'),
-    prevPick = slidePicker[0];
-labels.on('click',function(){
-    if($(slidePicker).attr('disabled') == 'disabled') return
-    labels.removeClass('selected')
-    $(this).toggleClass('selected')
+  prevPick = slidePicker[0]
+labels.on('click', function () {
+  if ($(slidePicker).attr('disabled') == 'disabled') return
+  labels.removeClass('selected')
+  $(this).toggleClass('selected')
 })
-slidePicker.on('click',function(){
-    let pickedIdx = $(this).index(),
-        pickedSlide = $('.slide').eq(pickedIdx),
-        card1 = pickedSlide[0].children[0],
-        card2 = pickedSlide[0].children[1];
-    if(this == prevPick) return;
-    function slideOut(whereTo,n1,n2){
-        slidePicker.attr('disabled', true)
-        gsap.timeline().to($('.slide')[$(prevPick).index()].children[n1],{x:`${whereTo}${window.innerWidth}`,duration:1,ease:'power4.out'});
-        gsap.timeline().to($('.slide')[$(prevPick).index()].children[n2],{x:`${whereTo}${window.innerWidth}`,duration:1,ease:'power4.out',delay: .25});
-        setTimeout(()=>{
-            $('.slide').hide()
-            pickedSlide.show()
-        },1500)
-    }
-    function slideIn(whereTo,c1,c2){
-        gsap.timeline({defaults: {duration:1,ease:'bounce.out',x:0}})
-        .fromTo(c1,{x:`${whereTo}${window.innerWidth}`},{delay:1.25})
-        .fromTo(c2,{x:`${whereTo}${window.innerWidth}`},{delay:-.25});
-        setTimeout(() => {
-            slidePicker.attr('disabled', false)
-        }, 3000);
-    }
-    if(pickedIdx > $(prevPick).index()){
-        slideOut(`-`,0,1)
-        slideIn(``,card1,card2)
-    }else{
-        slideOut(``,1,0)
-        slideIn(`-`,card2,card1)
-    }
-    prevPick = this;
+slidePicker.on('click', function () {
+  let pickedIdx = $(this).index(),
+    pickedSlide = $('.slide').eq(pickedIdx),
+    card1 = pickedSlide[0].children[0],
+    card2 = pickedSlide[0].children[1]
+  if (this == prevPick) return
+  function slideOut(whereTo, n1, n2) {
+    slidePicker.attr('disabled', true)
+    gsap
+      .timeline()
+      .to($('.slide')[$(prevPick).index()].children[n1], {
+        x: `${whereTo}${window.innerWidth}`,
+        duration: 1,
+        ease: 'power4.out'
+      })
+    gsap
+      .timeline()
+      .to($('.slide')[$(prevPick).index()].children[n2], {
+        x: `${whereTo}${window.innerWidth}`,
+        duration: 1,
+        ease: 'power4.out',
+        delay: 0.25
+      })
+    setTimeout(() => {
+      $('.slide').hide()
+      pickedSlide.show()
+    }, 1500)
+  }
+  function slideIn(whereTo, c1, c2) {
+    gsap
+      .timeline({ defaults: { duration: 1, ease: 'bounce.out', x: 0 } })
+      .fromTo(c1, { x: `${whereTo}${window.innerWidth}` }, { delay: 1.25 })
+      .fromTo(c2, { x: `${whereTo}${window.innerWidth}` }, { delay: -0.25 })
+    setTimeout(() => {
+      slidePicker.attr('disabled', false)
+    }, 3000)
+  }
+  if (pickedIdx > $(prevPick).index()) {
+    slideOut(`-`, 0, 1)
+    slideIn(``, card1, card2)
+  } else {
+    slideOut(``, 1, 0)
+    slideIn(`-`, card2, card1)
+  }
+  prevPick = this
 })
 //Projects section
 //Animation
-let projectsNavBarTl = gsap.timeline({defaults: {color: navLightColor}})
-.to('.marker-projects',{color: navLightColor})
-.to(bar,{backgroundColor: navLightColor})
-.to(contactBtn,{})
-.to(projectsBtn,{color: navPickedColor})
-.to(aboutBtn,{})
-.to(homeBtn,{})
+let projectsNavBarTl = gsap
+  .timeline({ defaults: { color: navLightColor } })
+  .to('.marker-projects', { color: navLightColor })
+  .to(bar, { backgroundColor: navLightColor })
+  .to(contactBtn, {})
+  .to(projectsBtn, { color: navPickedColor })
+  .to(aboutBtn, {})
+  .to(homeBtn, {})
 ScrollTrigger.create({
-    trigger: '.marker-projects',
-    start: 'top bottom',
-    scrub: true,
-    animation: projectsNavBarTl,
-});
+  trigger: '.marker-projects',
+  start: 'top bottom',
+  scrub: true,
+  animation: projectsNavBarTl
+})
 //Sphere of skills
 const skills = [
     'HTML5',
@@ -166,33 +199,58 @@ const skills = [
     'UML',
     'Git',
     'GitHub'
-],
-    tagCloud = TagCloud('.sphere',skills,{
+  ],
+  tagCloud = TagCloud('.sphere', skills, {
     radius: 235,
     maxSpeed: 'slow',
     initSpeed: 'slow',
     direction: 100,
-    keep: true,
-})
+    keep: true
+  })
 //Tilt animation
-VanillaTilt.init(document.querySelectorAll(".card"), {
-    max: 25,
-    speed: 800,
-    glare: true,
-    "max-glare": .125,
-});
+VanillaTilt.init(document.querySelectorAll('.card'), {
+  max: 25,
+  speed: 800,
+  glare: true,
+  'max-glare': 0.125
+})
 //Contact section
 //Navigation bar colors animation
-let contactNavBarTl = gsap.timeline({defaults: {color: navDarkColor}})
-.to('.marker-projects',{color: navLightColor})
-.to(bar,{backgroundColor: navDarkColor})
-.to(contactBtn,{color: navPickedColor})
-.to(projectsBtn,{})
-.to(aboutBtn,{})
-.to(homeBtn,{})
+let contactNavBarTl = gsap
+  .timeline({ defaults: { color: navDarkColor } })
+  .to('.marker-projects', { color: navLightColor })
+  .to(bar, { backgroundColor: navDarkColor })
+  .to(contactBtn, { color: navPickedColor })
+  .to(projectsBtn, {})
+  .to(aboutBtn, {})
+  .to(homeBtn, {})
 ScrollTrigger.create({
-    trigger: '.marker-contact',
-    start: 'top bottom',
-    scrub: true,
-    animation: contactNavBarTl,
-});
+  trigger: '.marker-contact',
+  start: 'top bottom',
+  scrub: true,
+  animation: contactNavBarTl
+})
+//Copy text 
+let trackBtn
+clipboardIcon.on('click', function () {
+    const copyText = this.parentNode.firstElementChild
+    navigator.clipboard.writeText(copyText.innerText);
+  if (trackBtn == this) return
+  if (
+    $(clipboardIcon[0].firstElementChild).attr('class') ===
+      'fa-solid fa-clipboard-check' ||
+    $(clipboardIcon[1].firstElementChild).attr('class') ===
+      'fa-solid fa-clipboard-check'
+  ) {
+    $(clipboardIcon[0].firstElementChild).toggleClass(
+      'fa-clipboard fa-clipboard-check'
+    )
+    $(clipboardIcon[1].firstElementChild).toggleClass(
+      'fa-clipboard fa-clipboard-check'
+    )
+    trackBtn = this
+    return
+  }
+  $(this.firstElementChild).toggleClass('fa-clipboard fa-clipboard-check')
+  trackBtn = this
+})
